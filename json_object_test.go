8108d64503e5f8a4138626a5ebddb18d14b35a40
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-18 11:36:02 +0700

    add TestJSONObject for test create object using struct

---
+++ json_object_test.go
@@ -0,0 +1,28 @@
+package gojson
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+type Customer struct {
+	FirstName  string
+	MiddleName string
+	LastName   string
+	Age        int
+	Married    bool
+}
+
+func TestJSONObject(t *testing.T) {
+	customer := Customer{
+		FirstName:  "Rifqi",
+		MiddleName: "Muhammad",
+		LastName:   "Aziz",
+		Age:        22,
+		Married:    false,
+	}
+
+	bytes, _ := json.Marshal(customer)
+	fmt.Println(string(bytes))
+}
