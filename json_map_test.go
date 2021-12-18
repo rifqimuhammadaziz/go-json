909815a3f48e89c83cfaa22170be04a6eec1b628
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-18 16:41:49 +0700

    add TestJSONMap for unpredictable JSON

---
+++ json_map_test.go
@@ -0,0 +1,31 @@
+package gojson
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestMapDecode(t *testing.T) {
+	jsonString := `{"id":"P001", "name":"iPhone 11 Pro Max 256GB Gold", "price":500000}`
+	jsonBytes := []byte(jsonString)
+
+	var result map[string]interface{}
+	json.Unmarshal(jsonBytes, &result)
+
+	fmt.Println(result)
+	fmt.Println(result["id"])
+	fmt.Println(result["name"])
+	fmt.Println(result["price"])
+}
+
+func TestMapEncode(t *testing.T) {
+	product := map[string]interface{}{
+		"id":    "P002",
+		"name":  "Lenovo Thinkpad",
+		"price": 7500000,
+	}
+
+	bytes, _ := json.Marshal(product)
+	fmt.Println(string(bytes))
+}
