4ef5f9f0b823a2ef1e51523b59e555ec1a11319b
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-18 12:07:05 +0700

    add TestDecode for convert JSON to text/go

---
+++ decode_json_test.go
@@ -0,0 +1,30 @@
+package gojson
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestDecode(t *testing.T) {
+	// json data
+	jsonString := `{"FirstName":"Rifqi","MiddleName":"Muhammad","LastName":"Aziz","Age":22,"Married":false}`
+
+	// convert json data to byte array
+	jsonBytes := []byte(jsonString)
+
+	// create object from struct
+	customer := &Customer{}
+
+	err := json.Unmarshal(jsonBytes, customer)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(customer)
+	fmt.Println(customer.FirstName)
+	fmt.Println(customer.MiddleName)
+	fmt.Println(customer.LastName)
+	fmt.Println(customer.Age)
+	fmt.Println(customer.Married)
+}
